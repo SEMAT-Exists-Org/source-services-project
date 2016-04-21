0ba49484a447f44bf16d9f9ecd11dd03a2e15a5f
Saulius Zukauskas <saulius.zukauskas@gmail.com>
2016-04-21 11:15:47 +0100

    swaping local port 8001 to 8002

--- application.js
+++ application.js
@@ -37,7 +37,7 @@ app.use('/', require('./apis/unused.js')());
 // Important that this is last!
 app.use(mbaasExpress.errorHandler());
 
-var port = process.env.FH_PORT || process.env.OPENSHIFT_NODEJS_PORT || 8001;
+var port = process.env.FH_PORT || process.env.OPENSHIFT_NODEJS_PORT || 8002;
 var host = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 var server = app.listen(port, host, function() {
   console.log("App started at: " + new Date() + " on port: " + port); 
